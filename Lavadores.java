c5357c94d6382485ec361e5b03de5d5e4a272764
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2022-10-05 20:33:12 -0600

    Create Lavadores.java

---
+++ Lavadores.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Lavadores {
+
+//Se crearon los ArrayList.
+
+ArrayList<String> nombre = new ArrayList<String>();
+ArrayList<String> edad = new ArrayList<String>();
+ArrayList<String> identificacion = new ArrayList<String>();
+ArrayList<String> fechaNacimiento = new ArrayList<String>();
+ArrayList<String> direccion = new ArrayList<String>();
+
+ArrayList<int> saldo = new ArrayList<int>();
+}
+
+
+//Los lavadores.com - Tarea1
+
+
+
+//Registrar clientes
+
+//Listar clientes
+
+//Crear cuenta
+
+//Realizar deposito
+
+//Realizar retiro
+
+//Mostrar saldo
