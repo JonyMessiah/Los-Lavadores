11442561cca52dc1e724a645b528d1c40371d802
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2022-10-07 13:21:29 -0600

    Correcion de errores y creacion final de registrar clientes
    Registrar clientes finalizado.

--- Lavadores.java
+++ Lavadores.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.*;
 
 
@@ -14,9 +15,10 @@ ArrayList<String> identificacion = new ArrayList<String>();
 ArrayList<String> fechaNacimiento = new ArrayList<String>();
 ArrayList<String> direccion = new ArrayList<String>();
 
-ArrayList<int> saldo = new ArrayList<int>();
-}
+ArrayList<Integer> saldo = new ArrayList<Integer>();
 
+static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+static PrintStream out = System.out;
 
 //Los lavadores.com - Tarea1
 
@@ -24,6 +26,23 @@ ArrayList<int> saldo = new ArrayList<int>();
 
 //Registrar clientes
 
+public static void registrarCliente(ArrayList<String> nombre, ArrayList<String> edad, ArrayList<String> fechaNacimiento, ArrayList<String> direccion, ArrayList<String> identificacion) throws IOException {
+out.println("Ingrese su nombre ");
+ nombre.add(in.readLine());
+ out.println("Ingrese su edad");
+ edad.add(in.readLine());
+ out.println("Ingrese su fecha de nacimiento");
+ fechaNacimiento.add(in.readLine());
+ out.println("Ingrese su identificación");
+identificacion.add(in.readLine());
+out.println("Ingrese su direccion");
+direccion.add(in.readLine());
+
+out.println("¡Felicidades! Su usuario ha sido creado");
+
+}
+
+}
 //Listar clientes
 
 //Crear cuenta
