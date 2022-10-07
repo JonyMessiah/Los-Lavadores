478a90e0dd8a37e2d1f993268c867102e46db035
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2022-10-07 13:45:58 -0600

    Correcion de erros y listar clientes
    Se corrigieron errores a la hora de recorrer los arrays en ambos metodos y se termino el metodo listar cliente.

--- Lavadores.java
+++ Lavadores.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.*;
 
 
@@ -15,8 +16,11 @@ ArrayList<String> fechaNacimiento = new ArrayList<String>();
 ArrayList<String> direccion = new ArrayList<String>();
 
 ArrayList<Integer> saldo = new ArrayList<Integer>();
-}
 
+static int numClientes;
+
+static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+static PrintStream out = System.out;
 
 //Los lavadores.com - Tarea1
 
@@ -24,8 +28,41 @@ ArrayList<Integer> saldo = new ArrayList<Integer>();
 
 //Registrar clientes
 
+public static void registrarCliente(ArrayList<String> nombre, ArrayList<String> edad, ArrayList<String> fechaNacimiento, ArrayList<String> direccion, ArrayList<String> identificacion) throws IOException {
+
+    out.println("Ingrese la cantidad de clientes que desea registrar.");
+    numClientes = Integer.parseInt(System.console().readLine());
+
+    for (int i = 0; i < numClientes; i++) {
+
+out.println("Ingrese su nombre ");
+ nombre.add(in.readLine());
+ out.println("Ingrese su edad");
+ edad.add(in.readLine());
+ out.println("Ingrese su fecha de nacimiento");
+ fechaNacimiento.add(in.readLine());
+ out.println("Ingrese su identificación");
+identificacion.add(in.readLine());
+out.println("Ingrese su direccion");
+direccion.add(in.readLine());
+
+out.println("¡Felicidades! Su usuario ha sido creado");
+
+    }
+
+}
+
+
 //Listar clientes
 
+public static void listarCliente(ArrayList<String> nombre, ArrayList<String> edad, ArrayList<String> fechaNacimiento, ArrayList<String> direccion, ArrayList<String> identificacion, int numClientes) throws IOException {
+    out.println("A continuación se presentaran los datos de los clientes registrados actualmente.");
+    
+    for (int i = 0; i < numClientes; i++) {
+        out.println("El nombre del cliente es: " +nombre.get(i) + " \n La edad del cliente es: " +edad.get(i) + " \n La fecha de nacimiento del cliente es: " +fechaNacimiento.get(i) + " \n La identificación del cliente es: " +identificacion.get(i) + " \n La direccion del cliente es: " +direccion.get(i));
+    }
+    
+    }
 //Crear cuenta
 
 //Realizar deposito
@@ -33,3 +70,5 @@ ArrayList<Integer> saldo = new ArrayList<Integer>();
 //Realizar retiro
 
 //Mostrar saldo
+
+}
